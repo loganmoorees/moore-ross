d6ddc3ae5e0bc7523309ee51385606f134e3ea19
Moore <793665514@qq.com>
2020-07-29 17:33:55 +0800

    commit

---
+++ moore-eureka-server/src/test/java/com/moore/MooreEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MooreEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
